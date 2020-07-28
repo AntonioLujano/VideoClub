045445cc759997eceb020a95e3b9afe807c5f44c
Alejandro Miguel Cruz <alfa.alx110@gmail.com>
2020-07-27 19:38:47 -0500

    Cambios

--- app/Http/Controllers/MasPopularesController.php
+++ app/Http/Controllers/MasPopularesController.php
@@ -18,7 +18,7 @@ class MasPopularesController extends Controller
     public function index(Request $request)
     {
         if (!$request) {
-            $peliculas = DB::select('select Pel.id_pelicula,Pel.titulo, Dir.nombre_dire,Dir.ap_paterno, Dir.ap_materno ,Gen.des_gen from Peliculas Pel, Directores Dir, Generos Gen where Gen.id_genero=Pel.id_genero and Pel.id_director=Dir.id_director order by Pel.id_pelicula ');
+            $peliculas = DB::select('select Pel.id_pelicula,Pel.titulo, Dir.nombre_dire,Dir.ap_paterno, Dir.ap_materno ,Gen.des_gen from Peliculas Pel, Directores Dir, Generos Gen where Gen.id_genero=Pel.id_genero and Pel.id_director=Dir.id_director order by Pel.id_pelicula');
             return view('MasPopulares.index',['peliculas' => $peliculas]);
         }else{
             if ($request) {
